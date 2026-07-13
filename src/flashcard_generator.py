4005402b15adeb58bbd6832b6984beb4d0671626
Nispand Tech <your-email@example.com>
2026-07-13 22:13:31 +0530

    Version 2-Rule Based smart Flashcards

--- src/flashcard_generator.py
+++ src/flashcard_generator.py
@@ -1,13 +1,55 @@
+# def generate_flashcards(cleaned_sentences):
+#     flashcards=[]
+
+#     for sentence in cleaned_sentences:
+#         flashcard= {
+#             "question": "Explain the following sentence.",
+#             "answer": sentence
+#         }
+
+#         flashcards.append(flashcard)
+
+
+#     return flashcards
+
+
 def generate_flashcards(cleaned_sentences):
     flashcards=[]
 
     for sentence in cleaned_sentences:
-        flashcard= {
-            "question": "Explain the following sentence.",
-            "answer": sentence
-        }
+        #rule based NLP
+        
+        ## check if the sentence follows the pattern "X is Y"
+
+        if " is " in sentence:
+            subject,definition=sentence.split(" is ",1)
+
+            flashcard={
+                "question": f"What is {subject}?",
+                "answer": definition.capitalize()
+            }
+
+        
+        ## check if the sentence follows the pattern "X are Y"
+
+        elif "are" in sentence:
+            subject,definition=sentence.split("are",1)
+
+            flashcard={
+                "question":f" What are {subject}?",
+                "answer":definition.capitalize()
+            }
+
+        
+        ## Fallback for all other sentences
+
+        else:
+            flashcard= {
+                "question":"Explain the following sentence:",
+                "answer":sentence
+            }
+
 
         flashcards.append(flashcard)
-
 
     return flashcards
